c85a4b1d072e6509900c0b548a23e6eb41e91664
James Pipe <github@jamespipe.com>
2015-02-09 12:24:43 +0000

    Added additional tests for

--- gol_main.py
+++ gol_main.py
@@ -246,9 +246,115 @@ def test_dead_world_stays_dead(myworld):
         print (output)
         print('Expected:')
         print (expected)    
+
+def test_two_edges_will_die(myworld):
+
+    test, output, expected = [], [], []
+
+    test.append(['#', ' ', ' '])
+    test.append(['#', ' ', ' '])
+    test.append(['#', '#', '#'])
     
+    output.append(['?', '?', '?'])
+    output.append(['?', '?', '?'])
+    output.append(['?', '?', '?'])
+    
+    expected.append([' ', ' ', ' '])
+    expected.append([' ', ' ', ' '])
+    expected.append([' ', ' ', ' '])
+    
+    for y in range(0,len(test)):
+        for x in range(0,len(test[y])):
+            myworld.cell(y,x).set_state(test[y][x])
+
+    myworld.next_frame()
+    
+    for y in range(0,len(test)):
+        for x in range(0,len(test[y])):
+            output[y][x] = myworld.cell(y,x).state()  
+    
+    if output == expected:
+        print('Two edges will be dead - PASSED')
+    else:
+        print('Two edges will be dead - FAILED')
+        print('output returned as:')
+        print (output)
+        print('Expected:')
+        print (expected)  
+
+def test_corners_will_stick(myworld):
+
+    test, output, expected = [], [], []
+
+    test.append(['#', ' ', '#'])
+    test.append([' ', ' ', ' '])
+    test.append(['#', ' ', '#'])
+    
+    output.append(['?', '?', '?'])
+    output.append(['?', '?', '?'])
+    output.append(['?', '?', '?'])
+    
+    expected.append(['#', ' ', '#'])
+    expected.append([' ', ' ', ' '])
+    expected.append(['#', ' ', '#'])
+    
+    for y in range(0,len(test)):
+        for x in range(0,len(test[y])):
+            myworld.cell(y,x).set_state(test[y][x])
+
+    myworld.next_frame()
+    
+    for y in range(0,len(test)):
+        for x in range(0,len(test[y])):
+            output[y][x] = myworld.cell(y,x).state()  
+    
+    if output == expected:
+        print('Test corners will stick - PASSED')
+    else:
+        print('Test corners will stick - FAILED')
+        print('output returned as:')
+        print (output)
+        print('Expected:')
+        print (expected)  
+
+def test_down_arrow_saturates(myworld):
+
+    test, output, expected = [], [], []
+
+    test.append(['#', ' ', '#'])
+    test.append([' ', '#', ' '])
+    test.append([' ', ' ', ' '])
+    
+    output.append(['?', '?', '?'])
+    output.append(['?', '?', '?'])
+    output.append(['?', '?', '?'])
+    
+    expected.append(['#', '#', '#'])
+    expected.append(['#', '#', '#'])
+    expected.append(['#', '#', '#'])
+    
+    for y in range(0,len(test)):
+        for x in range(0,len(test[y])):
+            myworld.cell(y,x).set_state(test[y][x])
+
+    myworld.next_frame()
+    
+    for y in range(0,len(test)):
+        for x in range(0,len(test[y])):
+            output[y][x] = myworld.cell(y,x).state()  
+    
+    if output == expected:
+        print('Test down arrow will saturate - PASSED')
+    else:
+        print('Test arrow will saturate - FAILED')
+        print('output returned as:')
+        print (output)
+        print('Expected:')
+        print (expected) 
+
+
 def run_random_simulation():
-    my_world = world(9)
+    my_world = world(70)
     my_world.print_grid()
     print("----created-----")
     my_world.randomize()
@@ -271,7 +377,11 @@ def run_test_suite():
     test_magic_world_becomes_saturated(myworld)
     test_saturated_world_dies(myworld)
     test_dead_world_stays_dead(myworld)
+    test_two_edges_will_die(myworld)
+    test_corners_will_stick(myworld)
+    test_down_arrow_saturates(myworld)
     
 if __name__ == "__main__":    
     
     run_test_suite()
+    run_random_simulation()
